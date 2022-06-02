bdc41b2d0c78c019892aa0342f63b06b441d2ed1
Tomás Silva <tomas-250@outlook.com>
2022-06-02 13:20:44 +0100

    Criação dos componentes e afins

---
+++ sopa-letras/src/constants/index.js
@@ -0,0 +1,15 @@
+export const PALAVRAS = [
+    "angular",
+    "react",
+    "vue",
+    "ember",
+    "backbone",
+    "bootstrap",
+    "meteor",
+    "html",
+    "javascript",
+    "css",
+    "svelte"
+];
+
+export const TIMEOUTGAME = 30;
